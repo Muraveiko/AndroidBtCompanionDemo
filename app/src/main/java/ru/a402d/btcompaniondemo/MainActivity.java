b4f99cd6d21a27eb61b420ea08be4cf96682711e
Muraveiko Oleg U <Muraveiko@users.noreply.github.com>
2022-07-07 10:42:51 +0300

    Add code from documentation "AS IS". Dialog not show

--- app/src/main/java/ru/a402d/btcompaniondemo/MainActivity.java
+++ app/src/main/java/ru/a402d/btcompaniondemo/MainActivity.java
@@ -1,14 +1,91 @@
 package ru.a402d.btcompaniondemo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothDevice;
+import android.companion.AssociationRequest;
+import android.companion.BluetoothDeviceFilter;
+import android.companion.CompanionDeviceManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.os.ParcelUuid;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.UUID;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+    final int SELECT_DEVICE_REQUEST_CODE = 555;
+
+    BluetoothDeviceFilter deviceFilter = new BluetoothDeviceFilter.Builder()
+            // Match only Bluetooth devices whose name matches the pattern.
+            .setNamePattern(Pattern.compile("My device"))
+            // Match only Bluetooth devices whose service UUID matches this pattern.
+            .addServiceUuid(new ParcelUuid(new UUID(0x123abcL, -1L)), null)
+            .build();
+
+    AssociationRequest pairingRequest = new AssociationRequest.Builder()
+            // Find only devices that match this request filter.
+            .addDeviceFilter(deviceFilter)
+            // Stop scanning as soon as one device matching the filter is found.
+            .setSingleDevice(true)
+            .build();
+
+    CompanionDeviceManager deviceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        deviceManager = (CompanionDeviceManager) getSystemService(Context.COMPANION_DEVICE_SERVICE);
+
+        findViewById(R.id.button).setOnClickListener(v -> callCompanion());
+    }
+
+    private void callCompanion(){
+        deviceManager.associate(pairingRequest, new CompanionDeviceManager.Callback() {
+            // Called when a device is found. Launch the IntentSender so the user can
+            // select the device they want to pair with.
+            @Override
+            public void onDeviceFound(IntentSender chooserLauncher) {
+                try {
+                    startIntentSenderForResult(
+                            chooserLauncher, SELECT_DEVICE_REQUEST_CODE, null, 0, 0, 0
+                    );
+                } catch (IntentSender.SendIntentException e) {
+                    Log.e("MainActivity", "Failed to send intent");
+                }
+            }
+
+            @Override
+            public void onFailure(CharSequence error) {
+                // Handle the failure.
+            }
+        }, null);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == SELECT_DEVICE_REQUEST_CODE && data != null) {
+            BluetoothDevice deviceToPair =
+                    data.getParcelableExtra(CompanionDeviceManager.EXTRA_DEVICE);
+            if (deviceToPair != null) {
+                // Continue to interact with the paired device.
+                ((TextView)findViewById(R.id.txtMac)).setText(deviceToPair.getAddress());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
